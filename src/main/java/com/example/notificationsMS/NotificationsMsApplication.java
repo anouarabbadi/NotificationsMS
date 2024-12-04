7ed16533628c4085f5fb3976a52489e03462781a
Anouar Abbadi <anouarabbadi@example.com>
2024-12-04 10:22:36 +0100

    Updates some files and add Docker Files

--- src/main/java/com/example/notificationsMS/NotificationsMsApplication.java
+++ src/main/java/com/example/notificationsMS/NotificationsMsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationsMsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationsMsApplication.class, args);
 	}
-
 }
